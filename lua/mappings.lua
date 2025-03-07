1b6d8227fce92a94055cedbe42c5e589c4a0dc54
Jean <jean.letessier@protonmail.com>
2025-03-07 18:22:45 +0100

    Initial config commit

---
+++ lua/mappings.lua
@@ -0,0 +1,15 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+
+map("n", "<C-h>", "<cmd>TmuxNavigateLeft<CR>", { desc = "Window left"})
+map("n", "<C-j>", "<cmd>TmuxNavigateDown<CR>", { desc = "Window down"})
+map("n", "<C-k>", "<cmd>TmuxNavigateUp<CR>", { desc = "Window up"})
+map("n", "<C-l>", "<cmd>TmuxNavigateRight<CR>", { desc = "Window right"})
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
