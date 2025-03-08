57ee2958a588ffbb23dca657824ed33184db7c5e
Jean <jean.letessier@protonmail.com>
2025-03-08 11:19:14 +0100

    Feat: Setup linter and mason to install required LSPs, linters and formatters automatically.

---
+++ lua/configs/mason-conform.lua
@@ -0,0 +1,4 @@
+require("mason-conform").setup({
+    -- List of formatters to ingnore during install
+    ignore_install = {},
+})
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -17,9 +17,44 @@ return {
     },
 
     {
+        "williamboman/mason-lspconfig.nvim",
+        event = "VeryLazy",
+        dependencies = { "nvim-lspconfig" },
+        config = function()
+            require("configs.mason-lspconfig")
+        end,
+    },
+
+    {
+        "mfussenegger/nvim-lint",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require("configs.lint")
+        end,
+    },
+
+    {
+        "rshkarin/mason-nvim-lint",
+        event = "VeryLazy",
+        dependencies = { "nvim-lint" },
+        config = function()
+            require("configs.mason-lint")
+        end,
+    },
+
+    {
         "stevearc/conform.nvim",
         event = "BufWritePre", -- uncomment for format on save
         opts = require("configs.conform"),
+    },
+
+    {
+        "zapling/mason-conform.nvim",
+        event = "VeryLazy",
+        dependencies = { "conform.nvim" },
+        config = function()
+            require("configs.mason-conform")
+        end,
     },
 
     {
