dbbc79c767226209b8183ec62e287dfbbd3ab618
Jean <jean.letessier@protonmail.com>
2025-03-08 17:17:04 +0100

    Feat: Added formatting with clang-format.

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,8 +1,17 @@
 local options = {
     formatters_by_ft = {
         lua = { "stylua" },
-        -- css = { "prettier" },
-        -- html = { "prettier" },
+        c_cpp = { "clang-format" }, -- Hack to force download, conform doesn't actually care about the "c_cpp"
+        c = { "clang_format" }, -- Actual name for configuration
+        cpp = { "clang_format" },
+    },
+
+    formatters = {
+        clang_format = {
+            prepend_args = {
+                "-style=file", -- Use the .clang-format file present in the project root.
+            },
+        },
     },
 
     format_on_save = {
