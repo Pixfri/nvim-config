ede8959a155d2fc2affadc16f27cf7a3f9c9b8ea
Jean <jean.letessier@protonmail.com>
2025-03-12 18:33:23 +0100

    Feat: Added Python support.

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -4,12 +4,29 @@ local options = {
         c_cpp = { "clang-format" }, -- Hack to force download, conform doesn't actually care about "c_cpp"
         c = { "clang_format" }, -- Actual name for configuration
         cpp = { "clang_format" },
+        python = { "isort", "black" },
     },
 
     formatters = {
+        -- C/C++
         clang_format = {
             prepend_args = {
                 "-style=file", -- Use the .clang-format file present in the project root.
+            },
+        },
+
+        -- Python
+        black = {
+            prepend_args = {
+                "--fast",
+                "--line-length",
+                "80",
+            },
+        },
+        isort = {
+            prepend_args = {
+                "--profile",
+                "black",
             },
         },
     },
