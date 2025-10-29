e7348c7bb7fb03a6220a96c6fe245a9b23355e23
= <jean.letessier@protonmail.com>
2025-10-29 13:03:38 +0100

    Update config for C++ support

---
+++ lua/configs/lsp/init.lua
@@ -0,0 +1,44 @@
+local on_attach = require("nvchad.configs.lspconfig").on_attach
+local on_init = require("nvchad.configs.lspconfig").on_init
+local capabilities = require("nvchad.configs.lspconfig").capabilities
+
+local servers = { "ts_ls", "clangd", "gopls", "pyright" }
+
+vim.lsp.enable(servers)
+
+-- for _, lsp in ipairs(servers) do
+-- 	local opts = {
+-- 		on_init = on_init,
+-- 		on_attach = on_attach,
+-- 		capabilities = capabilities,
+-- 	}
+--
+-- 	local exists, settings = pcall(require, "configs.lsp.server-settings." .. lsp)
+-- 	if exists then
+-- 		opts = merge_tb("force", settings, opts)
+-- 	end
+--
+vim.lsp.config("clangd", {
+	on_attach = function(client, bufnr)
+		client.server_capabilities.documentFormattingProvider = false
+		client.server_capabilities.documentRangeFormattingProvider = false
+		on_attach(client, bufnr)
+	end,
+	on_init = on_init,
+	capabilities = capabilities,
+})
+
+local config = {
+	virtual_text = true,
+	underline = true,
+	update_in_insert = false,
+	severity_sort = true,
+	float = {
+		focusable = false,
+		style = "minimal",
+		border = "single",
+		source = "always",
+	},
+}
+
+vim.diagnostic.config(config)
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,78 +1,116 @@
+local overrides = require "configs.overrides"
+
 return {
-  {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
-
-  {
-    "williamboman/mason.nvim",
-    ensure_installed = {
-      "rust-analyzer",
-    },
-  },
-
-  -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
-      require "configs.lspconfig"
+      require("nvchad.configs.lspconfig").defaults() -- nvchad defaults for lua
+      require "configs.lsp"
     end,
   },
 
-  -- test new blink
-  -- { import = "nvchad.blink.lazyspec" },
-
+  -- override plugin configs
   {
-  	"nvim-treesitter/nvim-treesitter",
-  	opts = {
-  		ensure_installed = {
-  			"vim", "lua", "vimdoc",
-       "html", "css", "rust"
-  		},
-  	},
+    "williamboman/mason.nvim",
+    opts = overrides.mason,
   },
 
   {
-    'mrcjkb/rustaceanvim',
-    version = '^4',
-    lazy = false,
-    ["rust-analyzer"] = {
-      cargo = {
-        allFeatures = true,
-      },
+    "nvim-treesitter/nvim-treesitter",
+    opts = overrides.treesitter,
+  },
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = overrides.nvimtree,
+  },
+
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = overrides.telescope,
+  },
+
+  -- add telescope-fzf-native
+  {
+    "telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      lazy = "false",
+      config = function()
+        require("telescope").load_extension "fzf"
+      end,
     },
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+    opts = overrides.cmp,
+  },
+
+  -- Additional plugins
+
+  -- escape using key combo (currently set to jk)
+  {
+    "max397574/better-escape.nvim",
+    config = function()
+      require("better_escape").setup()
+    end,
+    lazy = false,
   },
 
   {
     "mfussenegger/nvim-dap",
+    config = function()
+      require "configs.dap"
+    end,
     lazy = false,
   },
 
   {
-    'saecki/crates.nvim',
-    ft = {"rust", "toml"},
+    "rcarriga/nvim-dap-ui",
+    config = function()
+      require("dapui").setup()
+    end,
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+  },
+
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    config = function()
+      require("nvim-dap-virtual-text").setup()
+    end,
+    dependencies = { "mfussenegger/nvim-dap", "nvim-dap-ui" },
+  },
+
+  -- better bdelete, close buffers without closing windows
+  {
+    "ojroques/nvim-bufdel",
+    lazy = false,
+  },
+
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   event = "InsertEnter",
+  --   config = function()
+  --     require("copilot").setup(require "configs.copilot")
+  --   end,
+  -- },
+
+  {
+    "leoluz/nvim-dap-go",
+    ft = "go",
+    dependencies = "mfussenegger/nvim-dap",
     config = function(_, opts)
-      local crates = require('crates')
-      crates.setup(opts)
-      crates.show()
+      require("dap-go").setup(opts)
     end,
   },
 
   {
-    'hrsh7th/nvim-cmp',
-    opts = function()
-      local M = require "nvchad.configs.cmp"
-      table.insert(M.sources, {name = "crates"})
-      return M
-    end
-  },
-
-  {
-    'rust-lang/rust.vim',
-    ft = 'rust',
-    init = function ()
-      vim.g.rustfmt_autosave = 1
+    "stevearc/conform.nvim",
+    event = "BufWritePre",
+    config = function()
+      require "configs.conform"
     end,
   },
 
@@ -88,13 +126,13 @@ return {
       "nvim-lua/plenary.nvim",
       {
         "chrisgrieser/nvim-tinygit", -- optional: for Github PR view
-        dependencies = { "stevearc/dressing.nvim" }
+        dependencies = { "stevearc/dressing.nvim" },
       },
     },
     cmd = { "Fugit2", "Fugit2Diff", "Fugit2Graph" },
     keys = {
-      { "<leader>F", mode = 'n', "<cmd>Fugit2<CR>" }
-    }
+      { "<leader>F", mode = "n", "<cmd>Fugit2<CR>" },
+    },
   },
 
   {
